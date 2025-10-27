b44ef5d6dfeb81fc1c70d25a9fa80f39be0aff41
antoniohdz117 <antonio117.berrios@gmail.com>
2025-10-26 21:23:55 -0600

    PROCESOS DE AUTENTIFICACION POR ROLES

--- Back-end/hackathon/src/main/java/com/example/hackathon/model/Academico.java
+++ Back-end/hackathon/src/main/java/com/example/hackathon/model/Academico.java
@@ -7,23 +7,66 @@ import jakarta.persistence.*;
 @Table(name = "academico")
 public class Academico {
 
+    //INICIALIAZAMOS LA CLASE
+
+
     @Id
     @Column(name = "idusuario")
-    private Long idUsuario;
+    private Long idusuario;
 
     @Column(nullable = false, length = 13, unique = true)
     private String rfc;
 
     @Column(name = "idpuesto")
-    private Long idPuesto;
+    private Long idpuesto;
 
     @Column(name = "idarea")
-    private Long idArea;
+    private Long idarea;
 
     @OneToOne
-    @JoinColumn(name = "idUsuario", referencedColumnName = "idUsuario", insertable = false, updatable = false)
+    @JoinColumn(name = "idusuario", referencedColumnName = "idusuario", insertable = false, updatable = false)
     private Usuario usuario;
 
     // Getters y setters
-    // ...
+
+
+    public Long getIdusuario() {
+        return idusuario;
+    }
+
+    public void setIdusuario(Long idusuario) {
+        this.idusuario = idusuario;
+    }
+
+    public String getRfc() {
+        return rfc;
+    }
+
+    public void setRfc(String rfc) {
+        this.rfc = rfc;
+    }
+
+    public Long getIdpuesto() {
+        return idpuesto;
+    }
+
+    public void setIdpuesto(Long idpuesto) {
+        this.idpuesto = idpuesto;
+    }
+
+    public Long getIdarea() {
+        return idarea;
+    }
+
+    public void setIdarea(Long idarea) {
+        this.idarea = idarea;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
 }
--- Back-end/hackathon/src/main/java/com/example/hackathon/model/Usuario.java
+++ Back-end/hackathon/src/main/java/com/example/hackathon/model/Usuario.java
@@ -22,4 +22,76 @@ public class Usuario {
     @ManyToOne
     @JoinColumn(name ="idrol",nullable = false)
     private Rol idrol;
+
+    public int getIdusuario() {
+        return idusuario;
+    }
+
+    public void setIdusuario(int idusuario) {
+        this.idusuario = idusuario;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPaterno() {
+        return paterno;
+    }
+
+    public void setPaterno(String paterno) {
+        this.paterno = paterno;
+    }
+
+    public String getMaterno() {
+        return materno;
+    }
+
+    public void setMaterno(String materno) {
+        this.materno = materno;
+    }
+
+    public String getNumerofijo() {
+        return numerofijo;
+    }
+
+    public void setNumerofijo(String numerofijo) {
+        this.numerofijo = numerofijo;
+    }
+
+    public String getNumerocelular() {
+        return numerocelular;
+    }
+
+    public void setNumerocelular(String numerocelular) {
+        this.numerocelular = numerocelular;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getContrasenia() {
+        return contrasenia;
+    }
+
+    public void setContrasenia(String contrasenia) {
+        this.contrasenia = contrasenia;
+    }
+
+    public Rol getIdrol() {
+        return idrol;
+    }
+
+    public void setIdrol(Rol idrol) {
+        this.idrol = idrol;
+    }
 }
