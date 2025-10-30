1512b3efc306b2545d12825a3b292d62446d57c4
EAJMtz <alberto_martinezjav@hotmail.com>
2025-10-29 23:24:02 -0600

    Se añade modal rechazo academic

--- Front-End/front-end/src/components/academic/ReservationTable.jsx
+++ Front-End/front-end/src/components/academic/ReservationTable.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useState } from "react";
+import CancellationModal from "./CancellationModal";
 import "../../styles/ReservationTable.css";
 import { IconButton } from "@mui/material";
 import DeleteOutlineOutlinedIcon from '@mui/icons-material/DeleteOutlineOutlined';
@@ -6,7 +7,7 @@ import EditOutlinedIcon from '@mui/icons-material/EditOutlined';
 import FileDownloadOutlinedIcon from '@mui/icons-material/FileDownloadOutlined';
 
 //DATOS DE PRUEBA
-const reservations = [
+const initialReservations = [
   {
     id: 1,
     recinto: "Frontón Cerrado",
@@ -40,57 +41,92 @@ const reservations = [
 ];
 
 
-//TABLA QEUE MUESTRA LAS RESERVACIONES DE LOS FUNCIONARIOS
-
-
-//SE DEBE INTEGRAR UNA TABLA CON FILTROS CON MATERIAL UI CONTEMPLANDO LAS COLUMNAS DE ARRIBA
 const ReservationTable = () => {
-  return (
-    <div className="table-container">
-      <table className="reservation-table">
-        <thead>
-          <tr>
-            <th>Folio</th>
-            <th>Recinto</th>
-            <th>Categoría</th>
-            <th>Evento</th>
-            <th>Fecha</th>
-            <th>Hora inicio</th>
-            <th>Hora fin</th>
-              <th>Descargar</th>
-            <th>Opciones</th>
-          </tr>
-        </thead>
-        <tbody>
-          {reservations.map((r) => (
-            <tr key={r.id}>
-              <td>{r.id}</td>
-              <td>{r.recinto}</td>
-              <td>{r.categoria}</td>
-              <td>{r.evento}</td>
-              <td>{r.fecha}</td>
-              <td>{r.horaInicio}</td>
-              <td>{r.horaFin}</td>
-                <td>
-                    <IconButton className="btn-download">
-                        <FileDownloadOutlinedIcon />
-                    </IconButton>
-                </td>
-              <td>
-                  <IconButton className="btn-edit">
-                      <EditOutlinedIcon />
-                  </IconButton>
+    const [reservations, setReservations] = useState(initialReservations);
+    
+    const [isCancelModalOpen, setIsCancelModalOpen] = useState(false);
+    
+    const [selectedReservation, setSelectedReservation] = useState(null);
 
-                  <IconButton className="btn-delete">
-                      <DeleteOutlineOutlinedIcon />
-                  </IconButton>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </table>
-    </div>
-  );
+
+
+    const handleOpenCancelModal = (reservation) => {
+        setSelectedReservation(reservation);
+        setIsCancelModalOpen(true);
+    };
+
+    const handleCloseCancelModal = () => {
+        setIsCancelModalOpen(false);
+        setSelectedReservation(null);
+    };
+
+    const handleConfirmCancel = (id) => {
+        console.log(`Reservación ${id} CANCELADA y ELIMINADA!`);
+        
+        setReservations(prevReservations =>
+            prevReservations.filter(r => r.id !== id)
+        );
+        
+        handleCloseCancelModal(); 
+    };
+
+    // TABLA QEUE MUESTRA LAS RESERVACIONES DE LOS FUNCIONARIOS
+    return (
+        <div className="table-container">
+            <table className="reservation-table">
+                <thead>
+                    <tr>
+                        <th>Folio</th>
+                        <th>Recinto</th>
+                        <th>Categoría</th>
+                        <th>Evento</th>
+                        <th>Fecha</th>
+                        <th>Hora inicio</th>
+                        <th>Hora fin</th>
+                        <th>Descargar</th>
+                        <th>Opciones</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {reservations.map((r) => (
+                        <tr key={r.id}>
+                            <td>{r.id}</td>
+                            <td>{r.recinto}</td>
+                            <td>{r.categoria}</td>
+                            <td>{r.evento}</td>
+                            <td>{r.fecha}</td>
+                            <td>{r.horaInicio}</td>
+                            <td>{r.horaFin}</td>
+                            <td>
+                                <IconButton className="btn-download">
+                                    <FileDownloadOutlinedIcon />
+                                </IconButton>
+                            </td>
+                            <td>
+                                <IconButton className="btn-edit">
+                                    <EditOutlinedIcon />
+                                </IconButton>
+
+                                <IconButton
+                                    className="btn-delete"
+                                    onClick={() => handleOpenCancelModal(r)}
+                                >
+                                    <DeleteOutlineOutlinedIcon />
+                                </IconButton>
+                            </td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+
+            <CancellationModal
+                isOpen={isCancelModalOpen}
+                reservation={selectedReservation}
+                onConfirmCancel={handleConfirmCancel}
+                onCancel={handleCloseCancelModal}
+            />
+        </div>
+    );
 };
 
 export default ReservationTable;
